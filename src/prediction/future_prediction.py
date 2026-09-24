8e88d76c7af93f83f2b195b3c61c5bdc2df0cb3d
Venu Dadi <venudadi11@gmail.com>
2026-09-24 21:43:34 +0530

    Deleting the pseudo-models

--- src/prediction/future_prediction.py
+++
@@ -1,9 +0,0 @@
-def predict_future_purchase(df):
-
-    df["Purchase_Probability"] = (
-        (1 / (1 + df["Recency"])) * 0.5 +
-        (df["Frequency"] / (df["Frequency"].max() + 1)) * 0.3 +
-        (df["Monetary"] / (df["Monetary"].max() + 1)) * 0.2
-    )
-
-    return df
